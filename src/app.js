75926a413a25230c76bed2422436fb9226fa277f
Sudhanshu <sudhanshutalan@gmail.com>
2025-05-31 12:04:24 +0530

    updated databse app.js  file

--- src/app.js
+++ src/app.js
@@ -1,39 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/database.js");
+
 const app = express();
-
-// app.use("/", (req, res, next) => {
-//   console.log("handling main route");
-//   // res.send("Main home route");
-//   next();
-// });
-
-// app.get(
-//   "/user",
-//   (req, res, next) => {
-//     console.log("Handling the user route");
-//     // res.send("user route 1");
-//     next();
-//   },
-//   (re, res, next) => {
-//     console.log("handling user route 2");
-//     res.send("user route 2");
-//     next();
-//   }
-// );
-
-const { adminAuth } = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent");
-});
-
-app.get("/admin/deleteuser", (req, res) => {
-  res.send("User deleted successfully");
-});
-
 const port = 3000;
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+// calling connectDB function and handling promises
+connectDB()
+  .then(() => {
+    console.log("Connection successful");
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Connection unsuccessful. Some error occured....");
+  });
